c0651a2787cdec7e2aa8ba25c11b8d9cf8187b2c
Vana <147261987+Mewnenokawai201203@users.noreply.github.com>
2024-11-13 03:55:53 +0700

    Update urls.py
    thêm password và update

--- PythFilm/PythFilm/urls.py
+++ PythFilm/PythFilm/urls.py
@@ -109,6 +109,8 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('logout/', views.logout_view, name='logout'),
+    path('password/', views.updatePassword, name='password'),
+    path('update-user/', views.updateUser, name='update-user'),
     
     #Contact quan
     path('contact/', views.contact_view, name='contact'),
